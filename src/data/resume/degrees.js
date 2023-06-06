892e5f1f9abefb5566943e05e5830206b19f75fa
Sonja Olson <olson1sonja@gmail.com>
2023-06-06 15:32:11 -0700

    Update to skills and resume

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -1,15 +1,9 @@
 const degrees = [
   {
-    school: 'Stanford University',
-    degree: 'M.S. Computational and Mathematical Engineering',
-    link: 'https://stanford.edu',
+    school: 'Oregon State University',
+    degree: 'B.S. Computer Science',
+    link: 'https://oregonstate.edu',
     year: 2016,
-  },
-  {
-    school: 'University at Buffalo',
-    degree: 'B.S. Electrical Engineering, Computer Engineering',
-    link: 'https://buffalo.edu',
-    year: 2012,
   },
 ];
 
